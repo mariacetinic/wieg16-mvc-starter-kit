7b0725cc8865955b99ef8ac2d42c694544cbabd0
https://github.com/mariacetinic/html5.git <maria.cetinic@gmail.com>
2017-05-04 21:40:14 +0200

    försök till att skapa ett formulär med funktionen create

--- views/create.php
+++ views/create.php
@@ -46,11 +46,9 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
     <div class="container">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a
-            jumbotron and three supporting pieces of content. Use it as a starting point to create something more
-            unique.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        <h1>Här lägger du till recept!</h1>
+        <p>Välkommen! Älskar du att dela med dig?  Här har du möjlighet till att lägga till vilket recept du vill. Nedanför får du fylla i allt
+        du behöver fylla i. Kör ba kör!</p>
     </div>
 </div>
 
@@ -64,19 +62,25 @@
                 euismod. Donec sed odio dui. </p>
             <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-                euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
-                porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-                fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-6">
+
+            <h2>Lägg till recept här</h2>
+            <!-- Receptformuläret -->
+            <form action="/create-recipe" method="post">
+                <div class="form-group">
+                    <label>Namn:</label>
+                    <input type="text" class="form-control" id="recipe">
+                </div>
+                <div class="form-group">
+                    <label>Antal: </label>
+                    <input type="text" class="form-control" id="quantity">
+                </div>
+                <div class="form-group">
+                    <label>Svårighetsgrad: </label>
+                    <input type="text" class="form-control" id="difficulty">
+                </div>
+                <button type="submit" class="btn btn-default">Lägg till</button>
+            </form>
         </div>
     </div>
 
