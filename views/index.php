38c1c126789c8f9831c08728b4c3e2ddffacf417
https://github.com/mariacetinic/html5.git <maria.cetinic@gmail.com>
2017-05-11 16:22:03 +0200

    En uppdatera knapp tillagd

--- views/index.php
+++ views/index.php
@@ -15,8 +15,10 @@ include 'header.php'; //visar header och menyn
 		Gillar du inte det som erbjuds? Vilken tur att du har möjligheten att lägga upp ditt 
 		allra egna recept. Tryck nedan på knappen för att lägga till nya recept eller scrolla ned för att se redan tillagda.
 		Smaklig måltid!</p>
-		<p><a class="btn btn-primary btn-lg" href="http://mvc-app.dev/create" target="_blank" role="button">Lägg till recept &raquo;</a></p>
-	</div>
+		<p><a class="btn btn-primary btn-lg" href="http://mvc-app.dev/create" target="_blank" role="button">Lägg till recept</a></p>
+        <p><a class="btn btn-primary btn-lg" href="http://mvc-app.dev/edit" target="_blank" role="button">Uppdatera recept &raquo;</a></p>
+
+    </div>
 </div>
 
 <div class="container">
