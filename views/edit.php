9270876d361b7f9c927537d5e000ca6ce858ffd2
https://github.com/mariacetinic/html5.git <maria.cetinic@gmail.com>
2017-05-11 16:09:59 +0200

    lagt till header.php i filerna

--- views/edit.php
+++ views/edit.php
@@ -1,5 +1,6 @@
 <?php
 /* @var $recipe */
+include 'header.php'
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- views/index.php
+++ views/index.php
@@ -1,47 +1,10 @@
 <?php
 /* @var $recipes */
-
+include 'header.php'
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<link rel="icon" href="../../favicon.ico">
 
-	<title>Hungrig?</title>
 
-	<!-- Bootstrap core CSS -->
-	<link href="/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/css/app.css" rel="stylesheet">
-</head>
 
-<body>
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-			        aria-expanded="false" aria-controls="navbar">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">Startsida</a>
-			<ul class="nav navbar-nav">
-            	<li><a href="http://mvc-app.dev/create">Lägg till recept</a></li>
-        	</ul>
-		</div>
-		<div id="navbar" class="navbar-collapse collapse">
-			
-		</div><!--/.navbar-collapse -->
-	</div>	
-</nav>
 
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
