f2e02a65b747cd0eb10d2600cb6afe8b8e8df425
https://github.com/mariacetinic/html5.git <maria.cetinic@gmail.com>
2017-05-11 16:15:16 +0200

    lagt till en fil som visar footern på alla sidorna

---
+++ views/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maria
+ * Date: 2017-05-11
+ * Time: 16:12
+ */
+include 'header.php'
+?>
+
+<hr>
+
+	<footer>
+		<p>&copy; 2016 Company, Inc.</p>
+	</footer>
+</div> <!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="/js/vendor/jquery-3.2.1.min.js"></script>
+<script src="/js/vendor/bootstrap.min.js"></script>
+</body>
+</html>
